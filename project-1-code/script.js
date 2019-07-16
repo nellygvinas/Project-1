04975a388a4f226816b44c1c43a32cbcf307296d
Nelly Gonzalez-Vinas <nramos4510@gmail.com>
2019-07-16 13:38:38 -0400

    updated obstacles

--- project-1-code/script.js
+++ project-1-code/script.js
@@ -3,52 +3,19 @@ window.onload = function() {
 
  // Declare variables
   let currentGame, currentUnicorn, controller;
+
   
  //Grab canvas and set canvas context
   const canvas = document.querySelector("#my-canvas");
   const ctx = canvas.getContext("2d");
   
- // BACKGROUND FUNCTIONS -----------------------------------------------------------------
-  
-  let background = new Image();
-  background.src = '../images/flat-nature-art-fit.png';
-  
-    
-  let backgroundImage = {
-   img: background,
-   background_x: 0,
-   speed: -0.1,
-    
-   move: function() {
-   this.background_x += this.speed;
-   this.background_x %= canvas.width;
-   },
-    
-   draw: function() {
-     ctx.drawImage(this.img, this.background_x, 0);
-      if (this.speed < 0) {
-        ctx.drawImage(this.img, this.background_x + canvas.width, 0);
-      } else {
-          ctx.drawImage(this.img, this.background_x - this.img.width, 0);
-      }
-    },
-  };
-    
-    
-    function updateBackground() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      backgroundImage.draw();
-      backgroundImage.move();  
-      requestAnimationFrame(updateBackground);
-    
-    }
     
   // CLASSES ---------------------------------------------------------------------------
   
     class Game {
       constructor(){
         this.unicorn = {}; // unicorn => object
-        //this.obstacles = []; // obstacles => array
+        this.obstacles = []; // obstacles => array
         this.score = 0;
       }
     }
@@ -118,79 +85,73 @@ window.onload = function() {
           this.x = 870;
         }  
         this.drawUnicorn();
-      }
-     } // end of Unicorn class
+      } // end of moveUnicorn function.
+     } // end of Unicorn class.
 
 
-     class Obstacle {
-      constructor(){
-        this.x = 20;
-        this.velocityX = 0;
-        this.y = 450;
-        this.velocityY = 0;
-        this.width = 205;
-        this.height = 150;
-        this.jumping = true;
-        this.img = '../images/unicorn-idle.gif';
+    class Obstacle {
+      constructor(x,y,width,height){
+        this.x = 970;
+        this.velocityX = -5;
+        this.y = 150;
+        this.width = 100;
+        this.height = 300;
+        this.img = '../images/hoop.png';
       }
   
-      drawUnicorn(){
-        const unicornImg = new Image();
-        unicornImg.src = this.img;
-        // unicornImg.onload = function() {
-        ctx.drawImage(unicornImg, this.x, this.y, this.width, this.height)
-        // ctx.fillRect(50, 50, 100, 100)
-        // }
+      drawObstacle(){
+        let obstacleImg = new Image();
+        obstacleImg.src = this.img;
+        ctx.drawImage(obstacleImg, this.x, this.y, this.width, this.height)
       }
   
-      moveUnicorn(num){
-      //  ctx.clearRect(this.x, this.y, this.width, this.height);
-       
-        //If the up key is pressed and the unicorn is jumping, this happens.
-        //Note, the unicorn is initially set to jumping as true. Therefore, the below
-        //would only happen if the unicorn is already jumping and you press up again.
-        if (controller.up && this.jumping === false) {
-          // console.log("here")
-          this.velocityY -= 20;
-          this.jumping = true;
-         }
-        
-        //If the left key is pressed, velocity increases (or decreases) by this amount
-        if (controller.left) {
-          this.velocityX -= 0.5;
-         }
-         //If the right key is pressed, velocity increases (or decreases) by this amount
-        if (controller.right) {
-          this.velocityX += 0.5;
-         }
-        
-        // THEN THESE THINGS HAPPEN ONCE THE IFS ARE TESTED:
-         this.velocityY += 1.25;// gravity
-         this.x += this.velocityX; //
-         this.y += this.velocityY;
-         this.velocityX *= 0.9;// friction
-         this.velocityY *= 0.9;// friction
-
-      
-        // If the unicorn is falling below floor line. Sets jumping to false. 
-        // Unicorn starts at 450 on y, this makes sure it does not go past 450 (falling)
-        if (this.y > 450) {
-          this.jumping = false;
-          this.y = 450;
-          this.velocityY = 1.25;
-        }
-      
-        // If unicorn is going off the left of the screen
-        if (this.x < -205) {
-          this.x = 1024;
-        } else if (this.x > 870) { // Stops unicorn from going past right boundary.
-          this.x = 870;
-        }  
-        this.drawUnicorn();
+      moveObstacle(){
+        this.x += this.velocityX;  
+          
+        this.drawObstacle(this.x,this.y,this.width,this.height);
       }
-     } // end of Unicorn class
+      
+      } //End of Obstacle class.
+     
 
-
+ // BACKGROUND FUNCTIONS -----------------------------------------------------------------
+  
+ let background = new Image();
+ background.src = '../images/flat-nature-art-fit.png';
+ 
+   
+ let backgroundImage = {
+  img: background,
+  backgroundX: 0,
+  speed: -0.5,
+   
+  move: function() {
+   if (controller.left || controller.up || controller.right) {
+   this.backgroundX += this.speed;
+   this.backgroundX %= canvas.width;
+   }
+   //this.backgroundX += this.speed;
+   //this.backgroundX %= canvas.width;
+  },
+   
+  draw: function() {
+    ctx.drawImage(this.img, this.backgroundX, 0);
+     if (this.speed < 0) {
+       ctx.drawImage(this.img, this.backgroundX + canvas.width, 0);
+     } else {
+         ctx.drawImage(this.img, this.backgroundX - this.img.width, 0);
+     }
+   },
+ };
+   
+   
+   function updateBackground() {
+     ctx.clearRect(0, 0, canvas.width, canvas.height);
+     backgroundImage.draw();
+     backgroundImage.move();  
+     requestAnimationFrame(updateBackground);
+   
+   }
 
 
 
@@ -202,6 +163,7 @@ window.onload = function() {
     left:false,
     right:false,
     up:false,
+    unicornIdle: true,
     keyListener:function(event) {
       // When any key is pressed, the key state will be set to true. Conversely, if a key is released,
       //the key state will be false. Therefore, the controller values will become true
@@ -225,6 +187,8 @@ window.onload = function() {
     currentUnicorn.moveUnicorn(whereToGo);
      }
 
+
+
     }
   
   window.addEventListener("keydown", controller.keyListener)
@@ -232,7 +196,7 @@ window.onload = function() {
 
 
   document.onkeydown = function (e) {
-      console.log('Your e is: ', e); 
+      //console.log('Your e is: ', e); 
       let whereToGo = e.keyCode;
       //currentGame.unicorn.moveUnicorn(whereToGo); 
       currentUnicorn.moveUnicorn(whereToGo);
@@ -262,11 +226,15 @@ window.onload = function() {
       
       currentGame = new Game();
       currentUnicorn = new Unicorn();
+      currentObstacle = new Obstacle();
+
       currentGame.unicorn = currentUnicorn;
       // console.log(currentGame);
       currentUnicorn.drawUnicorn();
+      
+      currentGame.obstacle = currentObstacle;
+      currentObstacle.drawObstacle();
   
-      console.log(currentUnicorn);
       console.log("game started");
   
       update();
@@ -279,15 +247,17 @@ window.onload = function() {
     
     function update(){
       // ctx.clearRect(0, 0 , 1024, 600);
-      currentUnicorn.drawUnicorn();
+      //currentUnicorn.drawUnicorn();
       currentUnicorn.moveUnicorn();
-      // frames ++;
-      // updateBackground();
+      currentObstacle.drawObstacle();
+      currentObstacle.x -= 0.75
+      
+     
       requestAnimationFrame(update);
   
     }
   
-  }; // end of window on-load function
+  } // end of window on-load function
 
 
 
